54e1c2b1361d36483c17ca3976e29ad139d46fec
smahsan053 <45126166+smahsan053@users.noreply.github.com>
2023-10-29 14:36:23 +0000

    message configure done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// to view official ecma script documents visit following site
+// https://tc39.es/ecma262/#sec-intro
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3); // code readability should be high
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// Data Types in JS
+// number => 2 tp power 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol =. unique
+
+// object
+
+console.log(typeof "hitesh")
+console.log(typeof "age")
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
