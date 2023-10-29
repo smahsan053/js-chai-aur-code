75ded50c38cf44ed7ecc982ad75d3bde10b01703
smahsan053 <45126166+smahsan053@users.noreply.github.com>
2023-10-29 15:30:17 +0000

    message configure done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -5,12 +5,12 @@
 // let score = true;
 // let score = false;
 let score = "hitesh";
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof (valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -24,7 +24,7 @@ let isLoggedIn = 0
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -34,5 +34,44 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************** Opeartions **************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);  // add
+// console.log(2-2);  // subtract
+// console.log(2*2);  // multiply
+// console.log(2**2); // power
+// console.log(2**3); // power
+// console.log(2/3);  // divide
+// console.log(2%3);  // reminder
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + "2" + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+let num1, num2, num3
+// num1 = mum2 = num3 = 2 + 2 
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
