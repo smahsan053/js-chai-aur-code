2455cf98492b30f4274b5c052c4af7bd33348142
smahsan053 <45126166+smahsan053@users.noreply.github.com>
2023-11-05 12:26:07 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+let a = 10
+const b = 20
+var c = 30
+console.log(a);
+console.log(b);
+console.log(c);
+
+// {} is scope if it is with any function or if else or others statment
+if (true){
+    //scope
+    let d = 10
+    const e = 20
+    var f = 30
+}
+// console.log(d);
+// console.log(e);
+console.log(f);
