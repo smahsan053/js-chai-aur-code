c8da59ebc3c09a6fe9b6ff204c9ae9ab1b5504be
smahsan053 <45126166+smahsan053@users.noreply.github.com>
2023-11-08 15:20:40 +0000

    while loop

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,30 @@
+// while loop
+let index = 0
+while (index <= 10) {
+    console.log(`Value of Index is ${index}`);
+    index = index + 2;
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0 // variable initialization
+while (arr<myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr +1;
+}
+
+
+//  do while loop
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+let score2 = 11
+do {
+    console.log(`Score2 is ${score2}`);
+    score2++
+} while (score2 <= 10);
