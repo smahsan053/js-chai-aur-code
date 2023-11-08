9ec59ad03d5558273c363fed5685a5b1f860b8d5
smahsan053 <45126166+smahsan053@users.noreply.github.com>
2023-11-08 15:11:24 +0000

    for loop

--- 05_iterations/01_forloop.js
+++ 05_iterations/01_forloop.js
@@ -20,3 +20,31 @@ for (let i = 0; i < 10; i++) {
     }
   }
   
+  let myArray = ["flash", "batman", "superman"]
+
+  console.log(myArray.length);
+  for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+
+  }
+
+  // break and continue
+
+  // break
+  for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+      console.log(`Detected 5`);
+      break
+    }
+    console.log(`Value of i is ${index}`);
+  }
+
+// continue
+  for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+      console.log(`Detected 5`);
+      continue
+    }
+    console.log(`Value of i is ${index}`);
+  }
